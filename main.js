bb65899aa6735fe89d7665e5949ed27a585d8a4e
clayton wright <claytonwright@claytons-MacBook-Pro.local>
2022-06-21 15:10:26 -0400

    finished, all working

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 // Your code here.
-
-
-
-
+function getFirstName(user) {
+  return user.firstName
+}
+function getLastName(user) {
+  return user.lastName
+}
+function getFullName(user){
+  return `${user.firstName} ${user.lastName}`
+}
+function setFirstName(user,name){
+  user.firstName = name
+  return `${user.firstName} ${user.lastName}`
+}
+function setAge(user,age){
+  user.age = age
+  return `${user.firstName} ${user.lastName} age:${user.age}`
+}
+function giveBirthday (user){
+  if(user.age > 0){
+    user.age ++
+    return user 
+  }
+  if(user.age != true){
+    user.age = 1
+    return user
+  }
+}
+function marry (person1,person2){
+  person1.married = true
+  person2.married = true
+  person1.spouseName = `${person2.firstName} ${person2.lastName}`
+  person2.spouseName = `${person1.firstName} ${person1.lastName}`
+  console.log(person1,person2)
+  return person1,person2
+}
+function divorce (person1,person2){
+  person1.married = false
+  person2.married = false
+  delete person1['spouseName']
+  delete person2['spouseName']
+  return person1,person2
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
